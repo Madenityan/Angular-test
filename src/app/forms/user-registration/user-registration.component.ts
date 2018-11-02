b4d88bd95939314692c0bfc55954f57a6c9f7f0f
Madenityan <marinakorsyn@gmail.com>
2018-11-02 17:19:44 +0200

    renamed a component 'user-login', added component 'user-registration', used in the Guards test mode (CanActivate, CanDeactivate)

---
+++ src/app/forms/user-registration/user-registration.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { UserForm} from '../../models/userForm';
+import { HttpService} from '../../http.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css']
+})
+
+export class UserRegistrationComponent implements OnInit {
+
+  saved = false;
+
+  allControl: FormGroup;
+  public user: UserForm;
+
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.user = {
+      id: 0,
+      name: '',
+      email: '',
+      password: ''
+    };
+
+  this.allControl = this.formBuilder.group({
+    name: [''],
+    mail: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
+
+  // this.allControl.valueChanges.subscribe((value => console.log(value)));
+  }
+
+  // відправка даних на сервер
+  public submit() {
+    console.log(this.allControl, this.allControl.value);
+    this.httpService.register(this.allControl.value).subscribe((data: UserForm) => this.user = data);
+
+    this.allControl.reset();
+  }
+
+  canDeactivate(): boolean | Observable<boolean> {
+
+    if (!this.saved) {
+      return confirm('Вы хотите покинуть страницу?');
+    } else {
+      return true;
+    }
+  }
+
+}
